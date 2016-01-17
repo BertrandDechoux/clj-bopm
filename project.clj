cc7544bbcf33c2fcece2fd7bc64b46d8d998c51c
Bertrand Dechoux <BertrandDechoux@users.noreply.github.com>
2016-01-17 20:11:10 +0100

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/BertrandDechoux/clj-bopm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter "1.5.1"]
-                 [org.clojure/tools.cli "0.2.2"]]
-  :profiles {:dev {:plugins [[lein-midje "3.0.1"] [lein-marginalia "0.7.1"]]
-                   :dependencies [[midje "1.5.1"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter "1.9.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
+  :profiles {:dev {:plugins [[lein-midje "3.2"] [lein-marginalia "0.8.0"]]
+                   :dependencies [[midje "1.8.3"]]}}
   :main clj-bopm.cli)
